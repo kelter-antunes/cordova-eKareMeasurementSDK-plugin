4580104861f8abb64adf3dc34d1b6bd3bad66dc9
Miguel Antunes <miguel.aka.kelter@gmail.com>
2024-01-31 12:32:33 +0000

    Update after_plugin_add.js

--- hooks/after_plugin_add.js
+++ hooks/after_plugin_add.js
@@ -4,30 +4,25 @@ var fs = require('fs-extra');
 var path = require('path');
 
 module.exports = function (context) {
- 
     // Assuming you want to perform actions for the iOS platform
     var platform = 'ios';
 
-    // Check if the platform is added
-    if (context.opts.platforms.includes(platform)) {
-        var projectName = getProjectName(context);
+    var projectName = getProjectName(context);
 
-        if (projectName) {
-            var srcDir = path.join(context.opts.projectRoot, 'src', platform, 'Frameworks');
-            var destDir = path.join(context.opts.projectRoot, 'platforms', platform, projectName, 'Frameworks');
+    if (projectName) {
+        var srcDir = path.join(context.opts.projectRoot, 'src', platform, 'Frameworks');
+        var destDir = path.join(context.opts.projectRoot, 'platforms', platform, projectName, 'Frameworks');
 
-            // Ensure the destination directory exists
-            fs.ensureDirSync(destDir);
+        // Ensure the destination directory exists
+        fs.ensureDirSync(destDir);
 
-            // Copy files from src/ios/frameworks to the destination directory
-            fs.copySync(srcDir, destDir);
+        // Copy files from src/ios/frameworks to the destination directory
+        fs.copySync(srcDir, destDir);
 
-            console.log('Files copied successfully from src/' + platform + '/frameworks to platforms/' + platform + '/' + projectName + '/Frameworks');
-        } else {
-            console.error('Unable to determine Cordova project name. Please check your project setup.');
-        }
+        console.log('Files copied successfully from src/' + platform + '/frameworks to platforms/' + platform + '/' + projectName + '/Frameworks');
+    } else {
+        console.error('Unable to determine Cordova project name. Please check your project setup.');
     }
-  
 };
 
 function getProjectName(context) {
